253e9b003b2e495e202a22c242ad31015a687605
Ruslan Savustian <ruslan.elfbot@gmail.com>
2025-09-06 19:02:25 +0300

    last update of ai version 3.0

--- backend/src/ai/ai.module.ts
+++ backend/src/ai/ai.module.ts
@@ -9,13 +9,14 @@ import { Game } from '../game/game.entity';
 import { Order } from '../order/order.entity';
 import { OrderService } from '../order/order.service';
 import { TelegramService } from '../telegram/telegram-service';
+import { AccountService } from '../account/account.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([AiChatSession, AiMessage, Account, Game, Order]),
   ],
   controllers: [AiController],
-  providers: [AiService, OrderService, TelegramService],
+  providers: [AiService, OrderService, TelegramService, AccountService],
   exports: [AiService],
 })
 export class AiModule {}
