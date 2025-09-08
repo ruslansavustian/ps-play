0170d44f756b52f149050514289c3eead90bef81
Ruslan Savustian <ruslan.elfbot@gmail.com>
2025-09-08 15:33:00 +0300

    redux installed

--- frontend/src/app/[locale]/games/page.tsx
+++ frontend/src/app/[locale]/games/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { HomeGamesTable } from "@/components/tables/home-tables/home-games-table";
+import { SupportChat } from "@/components/chat/support-chat";
 
 export default function GamesPage() {
   return (
     <div>
       <HomeGamesTable />
+      <SupportChat />
     </div>
   );
 }
